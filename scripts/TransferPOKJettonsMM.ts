171b31a57278740e9964ad63a19469f7c254a492
qezarses-crypto <qezarses@gmail.com>
2025-12-24 23:47:50 +0500

    dele

--- scripts/TransferPOKJettonsMM.ts
+++ scripts/TransferPOKJettonsMM.ts
@@ -3,18 +3,18 @@ import { NetworkProvider } from '@ton/blueprint';
 import { CONTRACTADDRESS } from './!YOURCONTRACTADDRESS';
 
 export async function run(provider: NetworkProvider) {
-    const jettonWalletAddress = Address.parseRaw('0:9eb8abe2750e07bf4644ed33e7ecaeb510e8a42487c55beb27ee724ae26c0fdf');
+    const jettonWalletAddress = Address.parseRaw('0:b228b6c612aadd7c6af59e2284645040d2c030d3c95d5991091a5f99eb0a79a4');
     const contractAddress = Address.parse(CONTRACTADDRESS); 
-    const sender = Address.parse("UQBWOZUkRmnEf19c7KKwgY4q7FVjqTtOA19_1-97IiuU1Y_4");
+    const sender = Address.parse("UQAIzXA4iuqWwozllUQ1-fD_UIaCsdxhIkN5vDMza6KX2yZy");
 
     const comment = 'Transfer main jetton from MM';
     
-    const body = beginCell().storeUint(0xf8a7ea5, 32).storeInt(1735144534, 64).storeCoins(toNano('67939.23')).storeAddress(contractAddress).storeAddress(sender).storeBit(0).storeCoins(toNano('0.1')).storeBit(1).storeRef(beginCell().storeUint(0x00000000, 32).storeStringTail(comment).endCell()).endCell();
+    const body = beginCell().storeUint(0xf8a7ea5, 32).storeInt(1735144534, 64).storeCoins(toNano('1000')).storeAddress(contractAddress).storeAddress(sender).storeBit(0).storeCoins(toNano('0.2')).storeBit(1).storeRef(beginCell().storeUint(0x00000000, 32).storeStringTail(comment).endCell()).endCell();
 
 
     await provider.sender().send({
         to: jettonWalletAddress,
-        value: toNano('0.7'),
+        value: toNano('1'),
         bounce: true,
         body: body,
     });
